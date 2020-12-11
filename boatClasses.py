d542881ca66ca365dee593aa4d228a1811440c1f
giovap95 <giovanni.pagnon@gmail.com>
2020-12-11 15:15:46 +0100

    Moved Keel class to Boatclasses.py

--- boatClasses.py
+++ boatClasses.py
@@ -89,3 +89,32 @@ class Sea:
         self.cinematicViscosity = seaDict["cinematicViscosity"]
         self.gravityConstant = seaDict["gravityConstant"]
         return
+
+class Keel:
+
+    def __init__(self):
+        self.meanChord       = None
+        self.span            = None
+        self.area            = self.chord * self.span
+        self.aspectRatio     = self.span / self.meanChord
+        self.profile         = XFoil()
+        self.profile.airfoil = naca0012
+
+        return
+
+    def keel_lift(self, Boat, Sea, leewayAngle, boatSpeed):
+        cL2D = np.pi**2 / 10 * leewayAngle # in degrees
+        cL = cL2D / (1 + 2/self.aspectRatio) * leewayAngle
+        effectiveLeewayAngle = leewayAngle * np.cos(np.radians(Boat.rollAngle))
+        lift = 0.5 * Sea.waterDensity * boatSpeed * self.area * cL
+        return lift
+
+    def keel_resistance(self, Boat, Sea, leewayAngle, boatSpeed):
+        cL2D = np.pi**2 / 10 * leewayAngle # in degrees
+        cL = cL2D / (1 + 2/self.aspectRatio) * leewayAngle
+        cDvisc = 0
+        cDind  = cL**2 / (np.pi * self.aspectRatio)
+        cD = cDvisc + cDind
+        effectiveLeewayAngle = leewayAngle * np.cos(np.radians(Boat.rollAngle))
+        keelResistance = 0.5 * Sea.waterDensity * boatSpeed * self.area * cD
+        return keelResistance
