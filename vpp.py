32c204a22dd0be399ffcf82ae7c456430a4d6230
giovanni <giovanni.pagnon@gmail.com>
2020-12-28 18:53:00 +0100

    general fixes on vpp function

--- vpp.py
+++ vpp.py
@@ -5,28 +5,35 @@ Created on Sun Dec 27 10:36:00
 
 @author: giova
 """
-import scipy
+import scipy.optimize
 import numpy as np
 
 
 def vpp(Boat, Sails, Crew, Foil, Keel, Sea):
-    AWAvector = np.arange(20, 180,1)
-    TWS = 2
+    AWAvector = np.arange(20, 100,1)
+    TWAlist = []
+    TWS = 6.5
     BSlist = []
-    for AWA in AWAvector: 
-        a = 1 # boatSpeed lower limit for bisection
-        b = TWS # boatSpeed upper limit for bisection
-        BS = scipy.optimize.bisect(f,a,b, xtol=2e-12, rtol=0.1, args=(Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA))
-        print('AWA: ', AWA)
-        print('BS: ', BS)
-        print('Boat.rollAngle', Boat.rollAngle)
-        print('leeway angle', Boat.leewayAngle, '\n')
+    leeAngleList = []
+    rollList = []
+    for AWA in AWAvector:
+        BSguess = 5
+        BSlow = BSguess - 2
+        BSup = BSguess + 2
+        BS = scipy.optimize.bisect(f,BSlow,BSup, rtol=0.001, args=(Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA))
+
+        # Compute TWA for the polar graph:
+        TWA,_ = Sails.velocity_triangle(AWA, BS, TWS)
         BSlist.append(float(BS))
-    return BSlist, AWAvector
+        TWAlist.append(float(TWA))
+        leeAngleList.append(float(Boat.leewayAngle))
+        rollList.append(float(Boat.rollAngle))
+    return BSlist, TWAlist, leeAngleList, rollList
 
 
 def g(delta, boatSpeed, Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA):
     Boat.leewayAngle = delta
+
     Boat.rollAngle = 0
     maxCrewStability = Crew.crew_stability(Boat, Sea, boatSpeed)
     cMoment = 100000
@@ -38,25 +45,20 @@ def g(delta, boatSpeed, Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA):
         sMoment, sFwd, sLeeway          = Sails.sail_forces(Boat, Sea, boatSpeed, TWS, AWA)
         hMoment                         = Boat.hull_stability(Sea, boatSpeed)
         # hDrag                           = hull_resistance(boatSpeed, Sea)
-        cMoment = (sMoment + kMoment
+        cMoment = (sMoment
         - fMoment - hMoment)
     return kLeeway - sLeeway
 
 
 
 def f(boatSpeed, Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA):
-    a = 1
-    b = 10
-    Boat.leewayAngle = scipy.optimize.bisect(g,a,b, xtol=2e-12, rtol=0.1, args=(boatSpeed, Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA))
+    deltalow = -1
+    deltaup = 5
+    delta = (deltalow+deltaup)/2
+    Boat.leewayAngle = scipy.optimize.bisect(g,deltalow,deltaup, rtol=0.001, args=(boatSpeed, Boat, Sea, Crew, Sails, Keel, Foil, TWS, AWA))
 
     sMoment, sFwd, sLeeway          = Sails.sail_forces(Boat, Sea, boatSpeed, TWS, AWA)
     kLeeway, kLift, kMoment, kDrag  = Keel.keel_forces(Boat, Sea, boatSpeed)
     fMoment, fLift, fDrag, fLeeway  = Foil.foil_forces(Boat, Sea, boatSpeed)
     hDrag                           = Boat.hull_resistance(boatSpeed, Sea)
     return sFwd - hDrag - fDrag - kDrag
-
-
-
-
-
-
